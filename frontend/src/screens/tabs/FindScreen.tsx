406b8ba06f4a1acd309d2927f3f82d8155dbfba8
RAMET Benjamin <benjy.ramet@yahoo.fr>
2022-07-08 10:31:13 +0200

    Add search component to find stack

--- frontend/src/screens/tabs/FindScreen.tsx
+++ frontend/src/screens/tabs/FindScreen.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import FindStack from "../../navigation/FindStack";
+import Search from "../../components/Search";
 
 const FindScreen = () => {
-    return <FindStack />;
+    return (
+        <>
+            <Search />
+            <FindStack />
+        </>
+    );
 };
 
 export default FindScreen;
