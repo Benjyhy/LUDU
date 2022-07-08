b8ddcb94b0fe24129205888d783016e3d93933e0
RAMET Benjamin <benjy.ramet@yahoo.fr>
2022-07-08 10:50:34 +0200

    Arrow func

--- frontend/src/navigation/rootNavigation.ts
+++ frontend/src/navigation/rootNavigation.ts
@@ -2,8 +2,8 @@ import { createNavigationContainerRef } from "@react-navigation/native";
 
 export const navigationRef = createNavigationContainerRef();
 
-export function navigate(name, params) {
+export const navigate = (name, params) => {
     if (navigationRef.isReady()) {
         navigationRef.navigate(name, params);
     }
-}
+};
