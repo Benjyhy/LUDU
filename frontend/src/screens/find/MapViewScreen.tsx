6a7dd3c4413fa0807a7d0913f80ffa1ce5bb8f0e
RAMET Benjamin <benjy.ramet@yahoo.fr>
2022-07-08 12:26:05 +0200

    Add store mocked data + implement markers on map

--- frontend/src/screens/find/MapViewScreen.tsx
+++ frontend/src/screens/find/MapViewScreen.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { Center } from "native-base";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { Dimensions, StyleSheet } from "react-native";
+import stores from "../../mocks/markerStoreMockData";
 
 const MapViewScreen = () => {
     return (
         <Center>
-            <MapView style={styles.map}></MapView>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 50.62925,
+                    longitude: 3.057256,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            >
+                {stores.map((marker, index) => (
+                    <Marker
+                        key={index}
+                        coordinate={marker.latlng}
+                        title={marker.title}
+                    />
+                ))}
+            </MapView>
         </Center>
     );
 };
