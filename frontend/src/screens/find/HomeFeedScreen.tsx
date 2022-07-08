a06fd74027915b2bd3ef19de2ee23bd9e36604d2
RAMET Benjamin <benjy.ramet@yahoo.fr>
2022-07-08 16:44:59 +0200

    Create filter component and import it in home feed

--- frontend/src/screens/find/HomeFeedScreen.tsx
+++ frontend/src/screens/find/HomeFeedScreen.tsx
@@ -1,13 +1,25 @@
-import { Box, ScrollView, VStack, Heading, FlatList } from "native-base";
+import { Box, ScrollView, VStack, Heading, FlatList, Flex } from "native-base";
 import React from "react";
 import homeFeedMockData from "../../mocks/homeFeedMockData";
 import GameCard from "../../components/GameCard";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { TouchableOpacity } from "react-native";
+import Filter from "../../components/Filter";
+import { useState } from "react";
 
 const HomeFeedScreen = () => {
+    const [isActiveFilter, setIsActiveFilter] = useState(false);
     return (
         <Box>
             <VStack space={2.5} w="100%" px="3">
-                <Heading size="md">Games near you</Heading>
+                <Flex direction="row" justify="space-between">
+                    <Heading size="md">Games near you</Heading>
+                    <TouchableOpacity
+                        onPress={() => setIsActiveFilter(!isActiveFilter)}
+                    >
+                        <Ionicons name="ios-filter" size={24} color="black" />
+                    </TouchableOpacity>
+                </Flex>
                 <FlatList
                     data={homeFeedMockData}
                     numColumns={2}
@@ -16,6 +28,10 @@ const HomeFeedScreen = () => {
                     renderItem={({ item }) => <GameCard item={item} />}
                 />
             </VStack>
+            <Filter
+                active={isActiveFilter}
+                onFilterClick={(value) => setIsActiveFilter(value)}
+            />
         </Box>
     );
 };
