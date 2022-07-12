94a52bb3b63bd7b0f4fcbc37d89ce8f4420e10f8
RAMET Benjamin <ramet.benj@gmail.com>
2022-07-13 00:54:59 +0200

    Get current location wtih expo-location and display lat,lng

--- frontend/src/screens/find/HomeFeedScreen.tsx
+++ frontend/src/screens/find/HomeFeedScreen.tsx
@@ -1,14 +1,36 @@
-import { Box, ScrollView, VStack, Heading, FlatList, Flex } from "native-base";
-import React from "react";
+import { Box, ScrollView, VStack, Heading, FlatList, Flex, Text } from "native-base";
+import React, { useEffect, useState } from "react";
 import homeFeedMockData from "../../mocks/homeFeedMockData";
 import GameCard from "../../components/GameCard";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { TouchableOpacity } from "react-native";
 import Filter from "../../components/Filter";
-import { useState } from "react";
+import * as Location from 'expo-location';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentLocation } from "../../store/actions/currentLocationAction";
+import { MainAppState } from "../../models/states";
 
 const HomeFeedScreen = () => {
     const [isActiveFilter, setIsActiveFilter] = useState(false);
+    const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status === 'granted') {
+                let location = await Location.getCurrentPositionAsync({});
+                const { latitude, longitude } = location.coords;
+                dispatch(setCurrentLocation({ latitude, longitude }));
+                setLocation({ latitude, longitude })
+            }
+        })();
+
+    }, []);
+
+    // const currentLocation = useSelector((state: MainAppState) => state.currentLocation);
+    // setLocation(currentLocation)
+
     return (
         <Box>
             <VStack space={2.5} w="100%" px="3">
@@ -20,6 +42,7 @@ const HomeFeedScreen = () => {
                         <Ionicons name="ios-filter" size={24} color="black" />
                     </TouchableOpacity>
                 </Flex>
+                <Text>{location.latitude}{location.longitude}</Text>
                 <FlatList
                     data={homeFeedMockData}
                     numColumns={2}
