e402744e98694788f1659d83edee239e37b14966
RAMET Benjamin <ramet.benj@gmail.com>
2022-07-13 00:53:21 +0200

    Set up redux

---
+++ frontend/src/store/reducers/index.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import currentLocationReducer from "./currentLocationReducer";
+
+export default combineReducers({
+    currentLocation: currentLocationReducer,
+});
