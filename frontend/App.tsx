65a7ed34aab7628e2792b752551c314e5746aea2
RAMET Benjamin <ramet.benj@gmail.com>
2022-07-24 18:12:03 +0200

    Add animated logo when loading

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NativeBaseProvider, Text } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNav from "./src/navigation/Navigator";
 import { navigationRef } from "./src/navigation/rootNavigation";
 import { Provider } from "react-redux";
-import store from "./src/store"
+import store from "./src/store";
+import LogoAnim from "./src/components/LogoAnim";
 
 // Define the config
 // const config = {
@@ -40,14 +41,28 @@ import store from "./src/store"
 // };
 
 const App = () => {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 4000);
+    }, []);
+
+    if (!loading)
+        return (
+            <NativeBaseProvider>
+                <Provider store={store}>
+                    <NavigationContainer ref={navigationRef}>
+                        <StackNav />
+                    </NavigationContainer>
+                </Provider>
+            </NativeBaseProvider>
+        );
+
     return (
-        <NativeBaseProvider>
-            <Provider store={store}>
-                <NavigationContainer ref={navigationRef}>
-                    <StackNav />
-                </NavigationContainer>
-            </Provider>
-        </NativeBaseProvider>
+        <LogoAnim />
     );
 };
 
