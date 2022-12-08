a753852b6fc43dcb23df0ff12bdffdccbcb01acf
impsdc <pauliix.santamaria@gmail.com>
2022-12-08 16:36:31 +0100

    resolving dependances errors

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -16,59 +16,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @Get('/user/:filename')
-  // async serveUserImages(
-  //   @Param('filename') filename: string,
-  // ): Promise<StreamableFile> {
-  //   console.log(join(process.cwd(), appConfig().game.staticFolder));
-  //   const fileExist = checkIfFileOrDirectoryExists(
-  //     join(process.cwd(), appConfig().user.staticFolder, `${filename}.webp`),
-  //   );
-  //   if (!fileExist)
-  //     throw new HttpException(
-  //       {
-  //         status: HttpStatus.NOT_FOUND,
-  //         error: `${filename} not found`,
-  //       },
-  //       HttpStatus.NOT_FOUND,
-  //     );
-  //   const file = createReadStream(
-  //     join(
-  //       process.cwd(),
-  //       appConfig().user.staticFolder,
-  //       `/user/avatar/${filename}.webp`,
-  //     ),
-  //   );
-  //   return new StreamableFile(file);
-  // }
-
-  // @Get('/user/:filename')
-  // async serveGameImages(
-  //   @Param('filename') filename: string,
-  // ): Promise<StreamableFile> {
-  //   console.log(join(process.cwd(), appConfig().game.staticFolder));
-  //   const fileExist = checkIfFileOrDirectoryExists(
-  //     join(
-  //       process.cwd(),
-  //       appConfig().game.staticFolder,
-  //       `/game/thumbnail/${filename}.webp`,
-  //     ),
-  //   );
-  //   if (!fileExist)
-  //     throw new HttpException(
-  //       {
-  //         status: HttpStatus.NOT_FOUND,
-  //         error: `${filename} not found`,
-  //       },
-  //       HttpStatus.NOT_FOUND,
-  //     );
-  //   const file = createReadStream(
-  //     join(
-  //       process.cwd(),
-  //       appConfig().game.staticFolder,
-  //       `/game/thumbnail/${filename}.webp`,
-  //     ),
-  //   );
-  //   return new StreamableFile(file);
-  // }
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
 }
