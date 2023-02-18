05ffc33815383d6611ed7894cdac9e2acde802d4
RAMET Benjamin <ramet.benj@gmail.com>
2023-02-18 23:48:18 +0100

    Add query types and return types for rents

--- frontend/src/services/LUDU_API/rents.ts
+++ frontend/src/services/LUDU_API/rents.ts
@@ -1,37 +1,38 @@
+import { CreateRentPayload, Rent } from '../../models/states/Rent';
 import { emptySplitApi } from './api';
 
 const extendedApi = emptySplitApi.injectEndpoints({
   endpoints: (builder) => ({
-    createRent: builder.mutation({
+    createRent: builder.mutation<Rent, CreateRentPayload>({
       query: (rentToCreate) => ({
         url: '/rent',
         method: 'POST',
         body: rentToCreate,
       }),
     }),
-    getAllRents: builder.query({
+    getAllRents: builder.query<Rent[], void>({
       query: () => ({
         url: '/rent',
       }),
     }),
-    getRentById: builder.query({
-      query: (rentId) => ({
-        url: `/rent/${rentId}`,
+    getRentById: builder.query<Rent, { _id: string }>({
+      query: (rent) => ({
+        url: `/rent/${rent._id}`,
       }),
     }),
-    setRentToDone: builder.query({
-      query: (rentId) => ({
-        url: `/rent/done/${rentId}`,
+    setRentToDone: builder.query<Rent, { _id: string }>({
+      query: (rent) => ({
+        url: `/rent/done/${rent._id}`,
       }),
     }),
-    setRentToDelivered: builder.query({
-      query: (rentId) => ({
-        url: `/rent/delivered/${rentId}`,
+    setRentToDelivered: builder.query<Rent, { _id: string }>({
+      query: (rent) => ({
+        url: `/rent/delivered/${rent._id}`,
       }),
     }),
-    deleteRent: builder.mutation({
-      query: (rentToDeleteId) => ({
-        url: `/rent/${rentToDeleteId}`,
+    deleteRent: builder.mutation<Rent, { _id: string }>({
+      query: (rentToDelete) => ({
+        url: `/rent/${rentToDelete._id}`,
         method: 'DELETE',
       }),
     }),
