7df4bd51c8d79d2a02b255e3047a81d81b1b341b
RAMET Benjamin <ramet.benj@gmail.com>
2023-03-08 21:50:10 +0100

    Fetch zipcode thanks to lat, long when app is mounting

--- frontend/src/navigation/Navigator.tsx
+++ frontend/src/navigation/Navigator.tsx
@@ -5,31 +5,37 @@ import TabsStack from './TabsStack';
 import LoadingScreen from '../screens/LoadingScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/register/RegisterScreen';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentLocation } from '../store/actions/currentLocationAction';
 import * as Location from 'expo-location';
 import Phone from '../screens/register/Phone';
 import Avatar from '../screens/register/Avatar';
 import { RegisterContext } from '../utils/registerContext';
 import { UserCreate } from '../models/states/User';
+import { getZipCode } from '../services/geocodingService';
 
 const Stack = createNativeStackNavigator();
 
 const StackNav = () => {
-  //Before any navigation, get current position of the user and set it in redux
   const dispatch = useDispatch();
+  const [user, setUser] = useState<UserCreate | null>(null);
+  //Before any navigation, get current position of the user and set it in redux
   useEffect(() => {
     (async () => {
       const { status } = await Location.requestForegroundPermissionsAsync();
       if (status === 'granted') {
         const location = await Location.getCurrentPositionAsync();
         const { latitude, longitude } = location.coords;
-        dispatch(setCurrentLocation({ latitude, longitude }));
+        try {
+          const zipCode = await getZipCode(latitude, longitude);
+          dispatch(setCurrentLocation({ latitude, longitude, zipCode }));
+          console.log(zipCode);
+        } catch (e) {
+          console.log(e);
+        }
       }
     })();
   }, []);
-
-  const [user, setUser] = useState<UserCreate | null>(null);
 
   return (
     <RegisterContext.Provider value={{ user, setUser }}>
