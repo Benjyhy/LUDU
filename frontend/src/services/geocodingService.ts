112400ac59b7e6dfb6ca28e08dcf905cf1cc3e30
RAMET Benjamin <ramet.benj@gmail.com>
2023-03-08 21:50:24 +0100

    geoCoding service with google maps api

---
+++ frontend/src/services/geocodingService.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { GOOGLE_CLOUD_API_KEY } from '@env';
+
+export async function getZipCode(latitude, longitude) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_CLOUD_API_KEY}`;
+  try {
+    const response = await axios.get(url);
+    const result = response.data;
+    let zipCode = null;
+    for (const component of result.results[0].address_components) {
+      if (component.types.includes('postal_code')) {
+        zipCode = component.long_name;
+        break;
+      }
+    }
+    return zipCode;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
