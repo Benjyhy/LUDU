7100aaaeca6e1912f975a2d2a95b3e91692aed87
bnzorijana <bnzorijana@gmail.com>
2023-03-31 10:11:29 +0200

    Fix booking tabs filter

--- frontend/src/screens/tabs/BookingScreen.tsx
+++ frontend/src/screens/tabs/BookingScreen.tsx
@@ -7,30 +7,29 @@ import { useGetUserRentsQuery } from '../../services/LUDU_API/rents';
 import { FilterTypes } from '../../models/Filter';
 import { useSelector } from 'react-redux';
 import { MainAppState } from '../../models/states';
-import { RentStatus } from '../../models/states/Rent';
 
 const BookingTabsScreen = () => {
   const userLogged = useSelector((state: MainAppState) => state.user);
   const filterStatus = useSelector((state: MainAppState) => state.filterBookingsByStatus);
 
   const getParams = () => {
-    const params = { _id: userLogged.id };
+    const params = { _id: userLogged.id, status: '' };
     if (!filterStatus.filters.length) {
       return params;
     }
     return {
       ...params,
-      done: filterStatus.filters.includes(RentStatus.OVER),
-      delivered:
-        filterStatus.filters.includes(RentStatus.OVER) ||
-        filterStatus.filters.includes(RentStatus.INPROGRESS),
+      status: filterStatus.filters.join(),
     };
   };
+
   const {
     data: rents,
     isError,
     error,
-  } = useGetUserRentsQuery(getParams(), { refetchOnMountOrArgChange: true });
+  } = useGetUserRentsQuery(getParams(), {
+    refetchOnMountOrArgChange: true,
+  });
 
   if (isError) {
     console.log(error);
