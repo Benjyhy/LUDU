ba7c7ab0f4c05d0570af4d6312c72d68af30dd9a
Benjamin Ramet <ramet.benj@gmail.com>
2023-04-19 14:11:47 +0200

    Use query params to filter by category

--- server/src/modules/location/location.service.ts
+++ server/src/modules/location/location.service.ts
@@ -140,7 +140,7 @@ export class LocationService {
                         {
                           $match: {
                             categories: {
-                              $in: [categoryIds],
+                              $in: categoryIds,
                             },
                           },
                         },
